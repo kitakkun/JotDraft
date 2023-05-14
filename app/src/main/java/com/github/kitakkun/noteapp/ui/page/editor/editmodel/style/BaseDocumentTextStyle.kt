5c0cf1db0ccf5b48b981e026868260039d81b886
kitakkun <48154936+kitakkun@users.noreply.github.com>
2023-05-14 23:19:51 +0900

    change BaseDocumentTextStyle to enum class

--- app/src/main/java/com/github/kitakkun/noteapp/ui/page/editor/editmodel/style/BaseDocumentTextStyle.kt
+++ app/src/main/java/com/github/kitakkun/noteapp/ui/page/editor/editmodel/style/BaseDocumentTextStyle.kt
@@ -4,24 +4,22 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.sp
 
-sealed class BaseDocumentTextStyle(
+enum class BaseDocumentTextStyle(
     val color: Color,
     val fontSize: TextUnit,
     val letterSpacing: TextUnit,
 ) : AbstractDocumentTextStyle {
-    object Title : BaseDocumentTextStyle(
+    Title(
         color = Color.Black,
         fontSize = 25.sp,
         letterSpacing = 0.sp,
-    )
-
-    object Heading : BaseDocumentTextStyle(
+    ),
+    Heading(
         color = Color.Black,
         fontSize = 22.sp,
         letterSpacing = 0.sp,
-    )
-
-    object Body : BaseDocumentTextStyle(
+    ),
+    Body(
         color = Color.Black,
         fontSize = 20.sp,
         letterSpacing = 0.5.sp,
