1f4afcfaf812bb51d543e292939939014fe9f086
kitakkun <48154936+kitakkun@users.noreply.github.com>
2023-05-15 13:38:43 +0900

    add comments

--- app/src/main/java/com/github/kitakkun/noteapp/ui/page/editor/NoteEditorUiState.kt
+++ app/src/main/java/com/github/kitakkun/noteapp/ui/page/editor/NoteEditorUiState.kt
@@ -6,18 +6,23 @@ import com.github.kitakkun.noteapp.ui.page.editor.editmodel.EditorConfig
 import com.github.kitakkun.noteapp.ui.page.editor.editmodel.anchor.StyleAnchor
 
 data class NoteEditorUiState(
+    // document raw data
     val documentTitle: String = "Untitled",
     val content: TextFieldValue = TextFieldValue(),
 
+    // editor config
     val editorConfig: EditorConfig = EditorConfig(),
 
+    // styles
     val baseStyleAnchors: List<StyleAnchor> = emptyList(),
     val overrideStyles: List<StyleAnchor> = emptyList(),
 
+    // dialogs
     val showSelectBaseDocumentTextStyleDialog: Boolean = false,
     val showSelectColorDialog: Boolean = false,
     val showColorPickerDialog: Boolean = false,
 
+    // colors
     val availableColors: List<Color> = listOf(
         Color.Black,
         Color.Red, Color.Green, Color.Blue,
