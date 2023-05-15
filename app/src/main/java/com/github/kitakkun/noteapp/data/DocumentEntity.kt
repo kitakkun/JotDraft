199194fb5f0a298bcf7a1f1f496b11ae3ef7051d
kitakkun <48154936+kitakkun@users.noreply.github.com>
2023-05-15 17:58:36 +0900

    change primary key for DocumentEntity to UUID string

--- app/src/main/java/com/github/kitakkun/noteapp/data/DocumentEntity.kt
+++ app/src/main/java/com/github/kitakkun/noteapp/data/DocumentEntity.kt
@@ -2,10 +2,12 @@ package com.github.kitakkun.noteapp.data
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.util.UUID
 
 @Entity
 data class DocumentEntity(
-    @PrimaryKey val title: String,
+    @PrimaryKey val id: String = UUID.randomUUID().toString(),
+    val title: String,
     val content: String,
 //    val baseFormatSpans: List<IntRange>,
 //    val boldSpans: List<IntRange>,
