b21001c80ee21f183b36c817d352e92f65a4d58c
kitakkun <48154936+kitakkun@users.noreply.github.com>
2023-05-15 13:08:02 +0900

    create color picker dialog

---
+++ app/src/main/java/com/github/kitakkun/noteapp/ui/page/editor/dialog/ColorPickerDialog.kt
@@ -0,0 +1,106 @@
+package com.github.kitakkun.noteapp.ui.page.editor.dialog
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import com.github.kitakkun.noteapp.R
+import com.github.kitakkun.noteapp.ui.preview.PreviewContainer
+import com.github.skydoves.colorpicker.compose.AlphaSlider
+import com.github.skydoves.colorpicker.compose.BrightnessSlider
+import com.github.skydoves.colorpicker.compose.ColorEnvelope
+import com.github.skydoves.colorpicker.compose.HsvColorPicker
+import com.github.skydoves.colorpicker.compose.rememberColorPickerController
+
+@Composable
+fun ColorPickerDialog(
+    onDismiss: () -> Unit,
+    onCancel: () -> Unit,
+    onColorChange: (Color) -> Unit,
+) {
+    val controller = rememberColorPickerController()
+    Dialog(onDismissRequest = onDismiss) {
+        Column(
+            modifier = Modifier
+                .background(MaterialTheme.colorScheme.surface)
+                .scrollable(
+                    state = rememberScrollState(),
+                    orientation = Orientation.Vertical
+                )
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(4.dp),
+            ) {
+                IconButton(onClick = onCancel) {
+                    Icon(imageVector = Icons.Default.Close, contentDescription = null)
+                }
+                Button(onClick = onCancel) {
+                    Text(text = stringResource(R.string.ok))
+                    Spacer(modifier = Modifier.width(4.dp))
+                    Icon(imageVector = Icons.Default.Check, contentDescription = null)
+                }
+            }
+            HsvColorPicker(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(450.dp)
+                    .padding(10.dp),
+                controller = controller,
+                onColorChanged = { colorEnvelope: ColorEnvelope ->
+                    onColorChange(colorEnvelope.color)
+                }
+            )
+            AlphaSlider(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp)
+                    .height(35.dp),
+                controller = controller,
+            )
+            BrightnessSlider(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp)
+                    .height(35.dp),
+                controller = controller,
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun ColorPickerDialogPreview() = PreviewContainer {
+    ColorPickerDialog(
+        onDismiss = {},
+        onCancel = {},
+        onColorChange = {},
+    )
+}
