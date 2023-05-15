46b7a10fb6982e50ceef3a10b488cefda000f974
kitakkun <48154936+kitakkun@users.noreply.github.com>
2023-05-15 13:33:22 +0900

    add picked color indicator

--- app/src/main/java/com/github/kitakkun/noteapp/ui/page/editor/dialog/ColorPickerDialog.kt
+++ app/src/main/java/com/github/kitakkun/noteapp/ui/page/editor/dialog/ColorPickerDialog.kt
@@ -10,8 +10,10 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Close
@@ -21,15 +23,24 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import com.github.kitakkun.noteapp.R
 import com.github.kitakkun.noteapp.ui.preview.PreviewContainer
 import com.github.skydoves.colorpicker.compose.AlphaSlider
+import com.github.skydoves.colorpicker.compose.AlphaTile
 import com.github.skydoves.colorpicker.compose.BrightnessSlider
 import com.github.skydoves.colorpicker.compose.HsvColorPicker
 import com.github.skydoves.colorpicker.compose.rememberColorPickerController
@@ -41,6 +52,8 @@ fun ColorPickerDialog(
     onColorConfirm: (Color) -> Unit,
 ) {
     val controller = rememberColorPickerController()
+    var hexCode by remember { mutableStateOf("") }
+    var textColor by remember { mutableStateOf(Color.Transparent) }
     Dialog(onDismissRequest = onDismiss) {
         Column(
             modifier = Modifier
@@ -65,12 +78,31 @@ fun ColorPickerDialog(
                     Icon(imageVector = Icons.Default.Check, contentDescription = null)
                 }
             }
+            Text(
+                text = "#$hexCode",
+                color = textColor,
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            )
+
+            AlphaTile(
+                modifier = Modifier
+                    .size(80.dp)
+                    .clip(RoundedCornerShape(6.dp))
+                    .align(Alignment.CenterHorizontally),
+                controller = controller
+            )
             HsvColorPicker(
                 modifier = Modifier
                     .fillMaxWidth()
                     .height(450.dp)
                     .padding(10.dp),
                 controller = controller,
+                onColorChanged = { colorEnvelope ->
+                    hexCode = colorEnvelope.hexCode
+                    textColor = colorEnvelope.color
+                }
             )
             AlphaSlider(
                 modifier = Modifier
