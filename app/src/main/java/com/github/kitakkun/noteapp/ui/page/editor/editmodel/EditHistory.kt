e9b1680099cd4145469dd552009dadec81f4044a
kitakkun <48154936+kitakkun@users.noreply.github.com>
2023-07-02 16:35:43 +0900

    fix unresolved references

--- app/src/main/java/com/github/kitakkun/noteapp/ui/page/editor/editmodel/EditHistory.kt
+++ app/src/main/java/com/github/kitakkun/noteapp/ui/page/editor/editmodel/EditHistory.kt
@@ -1,8 +1,8 @@
 package com.github.kitakkun.noteapp.ui.page.editor.editmodel
 
 import androidx.compose.ui.text.input.TextFieldValue
-import com.github.kitakkun.noteapp.ui.page.editor.editmodel.anchor.BaseStyleAnchor
-import com.github.kitakkun.noteapp.ui.page.editor.editmodel.anchor.OverrideStyleAnchor
+import com.github.kitakkun.noteapp.data.model.BaseStyleAnchor
+import com.github.kitakkun.noteapp.data.model.OverrideStyleAnchor
 
 data class EditHistory(
     val content: TextFieldValue,
