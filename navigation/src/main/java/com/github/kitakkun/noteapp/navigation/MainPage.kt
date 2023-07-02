8dfbfce46ec934e1b2f507f39a9a4c5b455cc717
kitakkun <48154936+kitakkun@users.noreply.github.com>
2023-07-03 00:20:23 +0900

    fix: redundant routes

--- navigation/src/main/java/com/github/kitakkun/noteapp/navigation/MainPage.kt
+++ navigation/src/main/java/com/github/kitakkun/noteapp/navigation/MainPage.kt
@@ -4,9 +4,9 @@ import androidx.navigation.NavType
 import androidx.navigation.navArgument
 
 object MainPage : Page(route = "main") {
-    object Finder : Page(route = "main/finder")
+    object Finder : Page(route = "finder")
     object Editor : Page(
-        route = "main/editor",
+        route = "editor",
         arguments = listOf(
             navArgument(name = "documentId") {
                 type = NavType.StringType
