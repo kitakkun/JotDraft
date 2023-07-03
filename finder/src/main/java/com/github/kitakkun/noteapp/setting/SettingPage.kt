2d9003e92359c28d3792021e06a2563a6e019299
kitakkun <48154936+kitakkun@users.noreply.github.com>
2023-07-03 12:42:16 +0900

    feat: add rows in SettingPage

--- finder/src/main/java/com/github/kitakkun/noteapp/setting/SettingPage.kt
+++ finder/src/main/java/com/github/kitakkun/noteapp/setting/SettingPage.kt
@@ -2,6 +2,7 @@ package com.github.kitakkun.noteapp.setting
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.platform.LocalUriHandler
 import androidx.navigation.NavController
 import com.github.kitakkun.noteapp.navigation.SubPage
 
@@ -11,9 +12,13 @@ fun SettingPage(
     navController: NavController,
 ) {
     val isAutoSaveEnabled = viewModel.isAutoSaveEnabled.collectAsState()
+    val uriHandler = LocalUriHandler.current
     SettingView(
         isAutoSaveEnabled = isAutoSaveEnabled.value,
         onChangedAutoSaveEnabled = viewModel::setAutoSaveEnabled,
-        onClickOSSLicense = { navController.navigate(SubPage.License.route) }
+        onClickOSSLicense = { navController.navigate(SubPage.License.route) },
+        onClickDeveloper = {
+            uriHandler.openUri("https://twitter.com/kitakkun_pb")
+        }
     )
 }
--- finder/src/main/java/com/github/kitakkun/noteapp/setting/SettingView.kt
+++ finder/src/main/java/com/github/kitakkun/noteapp/setting/SettingView.kt
@@ -1,53 +1,143 @@
 package com.github.kitakkun.noteapp.setting
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowForwardIos
+import androidx.compose.material.icons.filled.EditNote
 import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.OpenInBrowser
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.github.kitakkun.noteapp.customview.preview.PreviewContainer
+import com.github.kitakkun.noteapp.finder.R
 
 @Composable
 fun SettingView(
     isAutoSaveEnabled: Boolean,
     onChangedAutoSaveEnabled: (Boolean) -> Unit,
     onClickOSSLicense: () -> Unit,
+    onClickDeveloper: () -> Unit,
 ) {
-    val itemPadding = PaddingValues(all = 16.dp)
-    LazyColumn {
+    LazyColumn(
+        modifier = Modifier.padding(vertical = 16.dp)
+    ) {
         item {
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier.padding(itemPadding),
+            SettingHeader(
+                label = stringResource(R.string.editor),
+                icon = Icons.Default.EditNote,
+            )
+        }
+        item {
+            SettingItem(
+                label = stringResource(R.string.auto_save),
+                modifier = Modifier.fillMaxWidth()
             ) {
-                Text(text = "Auto-Save")
-                Spacer(modifier = Modifier.weight(1f))
                 Switch(checked = isAutoSaveEnabled, onCheckedChange = onChangedAutoSaveEnabled)
             }
         }
         item {
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
+            SettingItem(
+                label = stringResource(R.string.show_line_number),
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Switch(checked = isAutoSaveEnabled, onCheckedChange = onChangedAutoSaveEnabled)
+            }
+        }
+        item {
+            SettingHeader(
+                label = stringResource(R.string.about),
+                icon = Icons.Default.Info,
+            )
+        }
+        item {
+            SettingItem(
+                label = stringResource(id = R.string.oss_licenses),
                 modifier = Modifier
                     .clickable {
                         onClickOSSLicense()
                     }
-                    .padding(itemPadding),
+                    .fillMaxWidth(),
             ) {
-                Text(text = "OSS Licenses")
-                Spacer(modifier = Modifier.weight(1f))
-                Icon(imageVector = Icons.Default.Info, contentDescription = null)
+                Icon(imageVector = Icons.Default.ArrowForwardIos, contentDescription = null)
+            }
+        }
+        item {
+            SettingItem(
+                label = stringResource(R.string.version),
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(text = "1.0.0")
+            }
+        }
+        item {
+            SettingItem(
+                label = stringResource(R.string.developer),
+                modifier = Modifier
+                    .clickable {
+                        onClickDeveloper()
+                    }
+                    .fillMaxWidth()
+            ) {
+                Text(text = stringResource(R.string.developer_name))
+                Icon(imageVector = Icons.Default.OpenInBrowser, contentDescription = null)
+            }
+        }
+    }
+}
+
+@Composable
+private fun SettingHeader(
+    label: String,
+    icon: ImageVector? = null,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Text(
+            text = label,
+            style = MaterialTheme.typography.headlineSmall,
+            modifier = modifier.padding(16.dp)
+        )
+        if (icon != null) {
+            Icon(imageVector = icon, contentDescription = null)
+        }
+    }
+}
+
+@Composable
+private fun SettingItem(
+    label: String,
+    modifier: Modifier = Modifier,
+    control: (@Composable RowScope.() -> Unit)? = null,
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+            .height(80.dp)
+            .padding(16.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+    ) {
+        Text(text = label, style = MaterialTheme.typography.bodyLarge)
+        if (control != null) {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                control()
             }
         }
     }
@@ -60,5 +150,6 @@ private fun SettingViewPreview() = PreviewContainer {
         isAutoSaveEnabled = true,
         onChangedAutoSaveEnabled = {},
         onClickOSSLicense = {},
+        onClickDeveloper = {},
     )
 }
